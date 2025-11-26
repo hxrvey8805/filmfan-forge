7ee8b87434ac965757e71ee0f6f2996656842a9a
hxrvey8805 <harvey_sandhu8805@outlook.com>
2025-11-26 22:08:47 +0000

    Change yellow lens to green (CineGeek green)

--- src/components/GlassesWithLenses.tsx
+++ src/components/GlassesWithLenses.tsx
@@ -9,9 +9,9 @@ export const GlassesWithLenses = ({ className, ...props }: GlassesWithLensesProp
   return (
     <div className={cn("relative inline-flex items-center justify-center", className)}>
       <Glasses className="relative z-10 w-full h-full" {...props} />
-      {/* Yellow lens (left) */}
+      {/* Green lens (left) */}
       <div 
-        className="absolute rounded-full bg-primary opacity-75 blur-[0.5px] pointer-events-none"
+        className="absolute rounded-full bg-green-500 opacity-75 blur-[0.5px] pointer-events-none"
         style={{ 
           left: '25%',
           top: '61%',
